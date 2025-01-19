b9fa1b875d518a218d7dec3e8b811990301ed2c7
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-18 22:24:12 -0700

    Add tailwind

--- playscattergories.client/src/App.jsx
+++ playscattergories.client/src/App.jsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
 import * as signalR from "@microsoft/signalr";
 
 function App() {
-  const [forecasts, setForecasts] = useState();
-
   useEffect(() => {
     console.log("starting connection");
     const connection = new signalR.HubConnectionBuilder()
@@ -32,57 +29,7 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    populateWeatherData();
-  }, []);
-
-  const contents =
-    forecasts === undefined ? (
-      <p>
-        <em>
-          Loading... Please refresh once the ASP.NET backend has started. See{" "}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{" "}
-          for more details.
-        </em>
-      </p>
-    ) : (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map((forecast) => (
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-
-  return (
-    <div>
-      <h1 id="tableLabel">Weather forecast!!!</h1>
-      <p>This component demonstrates fetching data from the server.</p>
-      {contents}
-    </div>
-  );
-
-  async function populateWeatherData() {
-    const response = await fetch("weatherforecast");
-    const data = await response.json();
-    setForecasts(data);
-  }
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
