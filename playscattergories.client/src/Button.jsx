99673d1348effbaa5d7bfe1bdaad83019deac224
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-22 16:57:17 -0700

    Style disabled button

--- playscattergories.client/src/Button.jsx
+++ playscattergories.client/src/Button.jsx
@@ -12,7 +12,7 @@ export default function Button({
   const buttonClasses = clsx(
     "mt-3 inline-flex w-full items-center justify-center rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
     !fullWidth && "sm:ml-3 sm:mt-0 sm:w-auto",
-    disabled && "opacity-50 cursor-not-allowed",
+    disabled || (loading && "opacity-50 cursor-not-allowed"),
     classes,
   );
   return (
